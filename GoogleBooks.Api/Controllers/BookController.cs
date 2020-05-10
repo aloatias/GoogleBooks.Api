013d10e009775c65ae2aa6c0e072a87499411727
aloatias <aloatias@hotmail.com>
2020-05-10 13:46:17 +0200

    wip

---
+++ GoogleBooks.Api/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GoogleBooks.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : Controller
+    {
+        [HttpGet]
+        [Route("GetAllBooks")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            return Ok("working");
+        }
+    }
+}
