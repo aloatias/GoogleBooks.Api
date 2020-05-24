e491343e3314591100294fedb8afaca932cfeeed
aloatias <aloatias@hotmail.com>
2020-05-24 15:09:22 +0200

    wip

--- GoogleBooks.Api/Services/BooksService.cs
+++ GoogleBooks.Api/Services/BooksService.cs
@@ -7,16 +7,18 @@ namespace GoogleBooks.Api.Services
 {
     public class BooksService : IBooksService
     {
-        private readonly IGoogleBooksClientService _googleBooksClient;
+        private readonly IGoogleBooksClientService _googleBooksClientService;
 
-        public BooksService(IGoogleBooksClientService googleBooksClient)
+        public BooksService(IGoogleBooksClientService googleBooksClientService)
         {
-            _googleBooksClient = googleBooksClient;
+            _googleBooksClientService = googleBooksClientService;
         }
 
         public async Task<Books> GetBooksByKeywordAsync(string keywords, int maxResults)
         {
-            return await _googleBooksClient.GetBooksByKeywordAsync(keywords, maxResults);
+            // TODO: Validate keywords before calling google client
+
+            return await _googleBooksClientService.GetBooksByKeywordAsync(keywords, maxResults);
         }
     }
 }
