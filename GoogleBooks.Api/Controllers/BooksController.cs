bbd8d7a8a24a38d44756427c06363510f7f68ce3
aloatias <aloatias@hotmail.com>
2020-05-24 12:54:46 +0200

    wip

---
+++ GoogleBooks.Api/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using GoogleBooks.Client.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GoogleBooks.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly IGoogleBooksClientService _googleBooksClient;
+
+        public BooksController(IGoogleBooksClientService googleBooksClientService)
+        {
+            _googleBooksClient = googleBooksClientService;
+        }
+
+        [HttpGet]
+        [Route("GetBooksByKeyword")]
+        public async Task<IActionResult> GetBooksByKeyword(string keywords, int maxResults)
+        {
+            var defaultBooksResult = await _googleBooksClient.GetBooksByKeywordAsync(keywords, maxResults);
+
+            return Ok(defaultBooksResult);
+        }
+    }
+}
