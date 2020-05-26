f7701115d8cdd636a64f7376fa02e93962cd3447
aloatias <aloatias@hotmail.com>
2020-05-26 13:44:00 +0200

    exception classes

--- GoogleBooks.Api/Dtos/Output/Exceptions/ErrorBase.cs
+++ GoogleBooks.Api/Dtos/Output/Exceptions/ErrorBase.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GoogleBooks.Api.Dtos.Output.Exceptions
+﻿namespace GoogleBooks.Api.Dtos.Output.Exceptions
 {
     public class ErrorBase
     {
+        public string ErrorMessage { get; private set; }
+
+        public ErrorBase(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
     }
 }
