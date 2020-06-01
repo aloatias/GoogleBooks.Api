28d1569bf24bad93bc554de2c8ec5a44b7c1ae1a
aloatias <aloatias@hotmail.com>
2020-06-01 15:15:21 +0200

    ExceptionMessages class to avoid magic strings

---
+++ GoogleBooks.Api/Helpers/ExceptionMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace GoogleBooks.Api.Helpers
+{
+    public static class ExceptionMessages
+    {
+        public const string InvalidKeyword = "You must at least enter a two character keyword";
+
+        private const string NotFound = "The Id: \" id \" was not found";
+
+        public static string GetNotFoundMessage(string id)
+        {
+            return NotFound.Replace("id", id);
+        }
+    }
+}
