14648ec34fd40b8e4a4b569a2b070648e015e931
aloatias <aloatias@hotmail.com>
2020-06-18 12:35:12 +0200

    comments

--- GoogleBooks.Client/Interfaces/IUrlFactory.cs
+++ GoogleBooks.Client/Interfaces/IUrlFactory.cs
@@ -4,10 +4,14 @@
     {
         string Url { get; }
 
+        /// <summary>
+        /// Sets a book details url from a book Id
+        /// </summary>
+        /// <param name="bookId"></param>
         void SetBookDetailsUrl(string bookId);
 
         /// <summary>
-        /// Gets a books list filtered by the keywords parameter with as many items per page as defined by the pageSize parameter and skipping every book item before the startIndex parameter
+        /// Sets a books catalog url filtered by the keywords parameter with as many items per page as defined by the pageSize parameter and skipping every book item before the startIndex parameter
         /// </summary>
         /// <param name="keywords"></param>
         /// <param name="pageSize"></param>
