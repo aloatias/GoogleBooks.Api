43ef093fdc2b738d3d0b0eca21fc67b9775738cb
aloatias <aloatias@hotmail.com>
2020-07-23 01:13:16 +0200

    Feautres/swashbuckle integration (#16)
    * Swagger impementation in progress
    
    * Swagger implementation working

--- GoogleBooks.Api/Startup.cs
+++ GoogleBooks.Api/Startup.cs
@@ -5,6 +5,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace GoogleBooks.Api
 {
@@ -33,6 +37,28 @@ namespace GoogleBooks.Api
             services.ConfigureApiServices(Configuration);
 
             services.AddAutoMapper(typeof(Startup));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Google Books API",
+                    Description = "A simple example of a ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Alvaro Atias",
+                        Email = "aloatias88@gmail.com",
+                        Url = new Uri("https://github.com/aloatias"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +68,17 @@ namespace GoogleBooks.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
