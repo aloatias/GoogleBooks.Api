429b7a5aefd9e8c36dcaa090e09735e9745bcefd
aloatias <aloatias@hotmail.com>
2020-07-23 01:26:49 +0200

    Minor refactoring on Startup

---
+++ GoogleBooks.Api/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace GoogleBooks.Api.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Google Books API",
+                    Description = "A simple example of a ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Alvaro Atias",
+                        Email = "aloatias88@gmail.com",
+                        Url = new Uri("https://github.com/aloatias"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under Google APIs",
+                        Url = new Uri("https://developers.google.com/books"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
--- GoogleBooks.Api/Startup.cs
+++ GoogleBooks.Api/Startup.cs
@@ -5,10 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using System;
-using System.IO;
-using System.Reflection;
 
 namespace GoogleBooks.Api
 {
@@ -38,27 +34,7 @@ namespace GoogleBooks.Api
 
             services.AddAutoMapper(typeof(Startup));
 
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Google Books API",
-                    Description = "A simple example of a ASP.NET Core Web API",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Alvaro Atias",
-                        Email = "aloatias88@gmail.com",
-                        Url = new Uri("https://github.com/aloatias"),
-                    }
-                });
-
-                // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath);
-            });
+            services.ConfigureSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
